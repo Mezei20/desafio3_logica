e8cef6f3eec227330fd9d9024ccabc21e7769a96
Wagner Mezei Filho <N0152152@netservicos.corp>
2026-04-08 16:16:47 -0300

    Projeto concluído. Partes comentadas utilizadas para testar o código, necessário retirar o comentário para interagir com o código

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-const input = ["true", "20"];
-let index = 0;
+// const input = ["Aragorn", "7500"]; - Área de interação, input de dados.
+// let index = 0;
 
-function gets() {
-return input[index++];
-}
+// function gets() {
+//     return input[index++];
+// } 
+
+// informações acima utilizadas para testar o código. Necessário retirar o comentário para teste
 
 function main() {
-  const hasPermission = gets().toLowerCase() === "true";
-  const age = parseInt(gets().trim());
-	if (!hasPermission) {
-    console.log("Acesso negado");
-  } else if (age < 18) {
-    console.log("Idade insuficiente");
-  } else {
-    console.log("Acesso permitido");
-  }
+let nome = gets()
+let xp = parseInt(gets())
+let nivel;
+
+switch (true) {
+    case xp < 1000:
+        nivel = "Ferro";
+        break;
+    case xp <= 2000:
+        nivel = "Bronze";
+        break;
+    case xp <= 5000:
+        nivel = "Prata";
+        break;
+    case xp <= 7000:
+        nivel = "Ouro";
+        break;
+    case xp <= 8000:
+        nivel = "Platina";
+        break;
+    case xp <= 9000:
+        nivel = "Ascendente";
+        break;
+    case xp <= 10000:
+        nivel = "Imortal";
+        break;
+    default:
+        nivel = "Radiante";
+        break;
 }
 
-main();
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
+}
+
+// main() - Necessário retirar o comentário para teste
